6f454f0a6fb61da6e7ddaf76cd7a50e6d3071c4e
kitsuyui <kitsuyui@kitsuyui.com>
2018-01-17 20:00:48 +0900

    build without netbsd/arm

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-go get github.com/jteeuwen/go-bindata/...
+go get -u github.com/jteeuwen/go-bindata/...
 go generate
 go get -d ./...
-CGO_ENABLE=0 gox -ldflags '-w -s' -output='build/myip_{{.OS}}_{{.Arch}}'
+CGO_ENABLE=0 \
+gox \
+-osarch='darwin/386 darwin/amd64' \
+-osarch='linux/386 linux/amd64 linux/arm' \
+-osarch='freebsd/386 freebsd/amd64 freebsd/arm' \
+-osarch='openbsd/386 openbsd/amd64' \
+-osarch='windows/386 windows/amd64' \
+-osarch='netbsd/386 netbsd/amd64' \
+-ldflags '-w -s' \
+-output='build/myip_{{.OS}}_{{.Arch}}'
