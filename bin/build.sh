ce4ce006abec503940d0b3d6439078d79444a702
kitsuyui <kitsuyui@kitsuyui.com>
2018-08-19 18:07:09 +0900

    Remove build target restriction

--- bin/build.sh
+++ bin/build.sh
@@ -2,12 +2,6 @@
 go get -d ./...
 CGO_ENABLE=0 \
 gox \
--osarch='darwin/386 darwin/amd64' \
--osarch='linux/386 linux/amd64 linux/arm' \
--osarch='freebsd/386 freebsd/amd64 freebsd/arm' \
--osarch='openbsd/386 openbsd/amd64' \
--osarch='windows/386 windows/amd64' \
--osarch='netbsd/386 netbsd/amd64' \
 -ldflags '-w -s' \
 -ldflags '-X main.version='"$BUILD_VERSION" \
 -output='build/myip_{{.OS}}_{{.Arch}}'
